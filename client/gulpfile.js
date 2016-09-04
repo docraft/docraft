29f24971ecbcf17910181bc11784784221cf58f6
docraft <docraft@qq.com>
2016-09-05 06:22:41 +0800

    pug, sass 环境搭建

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,7 +1,25 @@
 'use strict'
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug  = require('gulp-pug');
 
+
+// sass
+gulp.task('sass', function(){
+    return gulp.src('webroot/src/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('webroot/dist/css'))
+});
+
+// pug
+gulp.task('pug', function(){
+    return gulp.src('webroot/src/html/*.pug')
+        .pipe(pug())
+        .pipe(gulp.dest('webroot/dist/html'))
+});
+
+// default
 gulp.task('default', function(){
     console.log('default');
 });
